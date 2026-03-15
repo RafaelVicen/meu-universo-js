fb278f0358d2c47574b4f223dedf8f25c45709e4
Rafael vicente <rafaelvic1946@gmail.com>
2026-03-15 15:02:07 +0000

    new

---
+++ 01-estrutura-sequencial/03/recursosNativos.js
@@ -0,0 +1,22 @@
+//para poder exebir uma msg no console, nos digitamos o seguinte:
+console.log("Bem vindo a nave elementar")
+// alem do console.log, temos tbm o console.info
+console.info("Bem vindo a nave elementar")//retorna uma msg com a tonalidade diferente
+
+console.warn("Bem vindo a nave elementar")//serve para escrever algumas msg de urgencia
+
+console.error("Bem vindo a nave elementar")//quando queremos logar uma informacao de erro em uma 
+//execucao que ocorreu, porem no geral usamos o console.log//
+
+// temos tbm o alerte, que eh usado para nos exiber uma msg na tela
+//nada mais ele exibe uma caixa de msg para o usuario
+alert("Bem vindo a nave elementar")
+
+// temos tbm o confirmer, que eh uma caixa para nos confirmar ou nao confirmar uma msg
+confirm("Nave pronta para decolar, aguardando confirmacao")
+//ele consegue idenficar quando nos clicamos em confirmar ou cancelar
+//vamos ver o confirm melhor em outros exemplos
+// o quarto e ultimo que temos eh o prompt
+prompt("Qual o seu nome piloto")
+//o prompt ele exibe a msg que colocamos e coloca um campo de input de texto que eh para o usuario digitar
+//vamos ver o prompt melhor em outros exemplos
---
+++ 01-estrutura-sequencial/04/DeclaracaoVariaveis.js
@@ -0,0 +1,9 @@
+// As variaveis nos utilizamos para poder armazenar valores e utilizar 
+//futuramente no nosso codigo, porem para nos utilizar variavel, precisamos declarar
+//apartir da versao 6 do js, nos temos 3 tipos de declarar variaveis
+
+let spaceship = "FE SuperNova"// o que eu fiz? usei o lete para criar uma variavel
+//spaceship eh o nome da variavel, utilizei o igual que eh de atribuicao
+// e no final coloquei uma string entre aspas
+console.log(spaceship)//aqui eu estou a imprimir o conteudo dessa variavel, ou seja
+//estou a mostrar na tela o que foi feito acima
